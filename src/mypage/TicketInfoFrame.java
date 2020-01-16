0d265b3fbc76f3b91a94c2d55caec304a3accff8
compass-121 <58839593+compass-121@users.noreply.github.com>
2020-01-16 17:34:29 +0900

    d

--- src/mypage/TicketInfoFrame.java
+++ src/mypage/TicketInfoFrame.java
@@ -204,7 +204,7 @@ public class TicketInfoFrame extends JFrame {
 		ReserveDBMgr rvMgr = new ReserveDBMgr();
 		//fff = rvMgr.selectSeat();
 		ArrayList<Reserve1> rvList = rvMgr.reveResult();
-		
+		System.out.println(rvList.size());
 //		for (Reserve1 reserve1 : rvList) {
 //			System.out.println(reserve1.toString());
 //		}
